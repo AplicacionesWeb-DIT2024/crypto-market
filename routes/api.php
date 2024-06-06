0997d6a04833c574425274c0c6bac315c68d610c
ramirocanario1 <ramirocanario1@gmail.com>
2024-06-06 17:50:05 -0300

    chore: eliminacion de imports innecesarios

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\usuarioController;
 use App\Http\Controllers\api\movimientoController;
 use App\Http\Controllers\api\criptomonedaController;
 use App\Http\Controllers\api\transaccionController;
-use App\Http\Controllers\api\comentarioController;
 
 //  Usuarios
 Route::get('/usuarios', [usuarioController::class, 'index']);
